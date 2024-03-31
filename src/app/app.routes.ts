d342388217efad0a744f40506d67bd5604ddde94
Say <reubenlouis64@gmail.com>
2024-03-31 17:38:32 +0800

    Add routing to base components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./core/components/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
+  },
+  {
+    path: 'manager',
+    loadComponent: () =>
+      import('./core/components/manager/manager.component').then(
+        (m) => m.ManagerComponent
+      ),
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('./core/components/user/user.component').then(
+        (m) => m.UserComponent
+      ),
+  },
+  {
+    path: 'pos',
+    loadComponent: () =>
+      import('./core/components/pos/pos.component').then((m) => m.PosComponent),
+  },
+];
